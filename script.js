92c7f01aab5e241b285fa69682febd4a334e5d7b
Karthik Kandi <101356427+karthik6624@users.noreply.github.com>
2023-05-31 00:37:28 +0530

    Search Button included

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Being zero")
+function searchWord() {
+    var word = document.getElementById("searchBox").value;
+    word = word.toLowerCase();
+    var pageContent = document.body.innerText.toLowerCase();
+    var searchResult = pageContent.indexOf(word);
+    if (searchResult !== -1) {
+      alert("The word \"" + word + "\" was found on the page.");
+    } else {
+      alert("The word \"" + word + "\" was not found on the page.");
+    }
+  }
