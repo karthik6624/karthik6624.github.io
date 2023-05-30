f347cf2405a48a8f8ef482847d3587e6b58014e5
Karthik Kandi <101356427+karthik6624@users.noreply.github.com>
2023-05-31 00:56:43 +0530

    ENCAPSULATE

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-function searchWord() {
-    // Get the input word from the search box
-    var word = document.getElementById("searchBox").value;
-    word = word.toLowerCase();
-    var pageContent = document.body.innerHTML;
-    var regex = new RegExp(word, 'gi');
-    var highlightedContent = pageContent.replace(regex, "<span class='highlight'>$&</span>");
-    document.body.innerHTML = highlightedContent;
-  }
+const p=document.getElementById('body')
+function searchWord(){
+    console.log("search");
+}
