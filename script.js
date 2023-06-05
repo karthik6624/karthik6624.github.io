4a5f96ea001a40be3112ec931d083d4406721e80
Karthik Kandi <101356427+karthik6624@users.noreply.github.com>
2023-06-05 21:51:23 +0530

    Changes made to html

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var data1={image:"IMG_20210317_173101.jpg",name:"Siddarth",designation:"Software Intern"};
-var data2={image:"IMG_20210317_173101.jpg",name:"Manoj",designation:"Architect"};
+var data1={image:"./Images/IMG_20210317_173101.jpg",name:"Siddarth",designation:"Software Intern"};
+var data2={image:"./Images/IMG_20210317_173101.jpg",name:"Manoj",designation:"Architect"};
 var isdata1=true;
 var displaydata;
 var GenerateNewUser=function(){
-    if(ifdata1){
+    if(isdata1){
         displaydata=data2;
         isdata1=false;
     }
