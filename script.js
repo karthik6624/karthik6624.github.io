87641faa05e5cfb78b962c97ccf46efd5d319100
Karthik Kandi <101356427+karthik6624@users.noreply.github.com>
2023-06-05 22:48:40 +0530

    New Image Added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var data1={image:"IMG_20210317_173101.jpg",name:"Karthik Kandi",designation:"Software Intern"};
-var data2={image:"Karthik.jpg",name:"Manoj",designation:"Architect"};
+var data2={image:"IMG_2841.JPG",name:"Manoj",designation:"Architect"};
 var isdata1=true;
 var displaydata;
 var newuser=function(){
